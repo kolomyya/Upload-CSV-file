f2e5f309680827e078215628b19049b86ae8e4c9
kolomyya <andriyzn4@gmail.com>
2020-01-15 02:33:39 -0600

    testing script added

---
+++ vars/uploadTest.groovy
@@ -0,0 +1,21 @@
+node { 
+    deleteDir() 
+    stage("upload") { 
+        def inputFile = input message: 'Upload file', parameters: [file(name: 'restaurantConfigCSV')] 
+        new hudson.FilePath(new File("$workspace/restaurantConfigCSV")).copyFrom(inputFile) 
+        inputFile.delete() 
+    }
+    
+
+    stage("checkout") { 
+     echo fileExists('restaurantConfigCSV').toString() 
+    stage("read") {
+        def filenames = readFile 'restaurantConfigCSV'
+        def filenameArray = filenames.split(",")
+        for(int i = 0; i < filenameArray.size(); i++) {
+        def file = filenameArray[i]
+        echo file
+    }   
+    }    
+    } 
+    } 
