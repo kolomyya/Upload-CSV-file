321ed131f099f108c4c4fe304434f4d57d828ff6
kolomyya <44345671+kolomyya@users.noreply.github.com>
2020-01-16 13:46:59 -0600

    commit

--- src/com/lib/uploadTest.groovy
+++ src/com/lib/uploadTest.groovy
@@ -12,12 +12,14 @@ node {
         new hudson.FilePath(new File("$workspace/restaurantConfigCSV")).copyFrom(inputFile) 
         inputFile.delete() 
     }
-    
-
+    stage("find") {
+        def pomPath = findFiles(glob: "**/restaurantConfigCSV")[0].path
+        echo new File(env.WORKSPACE, pomPath).getParent() +"\restaurantConfigCSV"
+    }
     stage("checkout") { 
-     echo fileExists("$workspace/restaurantConfigCSV".toString() 
+     echo fileExists('restaurantConfigCSV').toString() 
     stage("read") {
-        def filenames = readFile "$workspace/restaurantConfigCSV"
+        def filenames = readFile 'restaurantConfigCSV'
         def filenameArray = filenames.split(",")
         for(int i = 0; i < filenameArray.size(); i++) {
         def file = filenameArray[i]
