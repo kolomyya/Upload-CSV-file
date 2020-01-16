711be6dc6023119717907c1d35cab750e564f84d
kolomyya <44345671+kolomyya@users.noreply.github.com>
2020-01-16 14:34:35 -0600

    changed path

--- src/com/lib/uploadTest.groovy
+++ src/com/lib/uploadTest.groovy
@@ -13,7 +13,7 @@ node {
         inputFile.delete() 
     }
     stage("find") {
-        pomPath = findFiles(glob: "**/$pomFile")[0].path
+        pomPath = findFiles(glob: "**/$restaurantConfigCSV")[0].path
         env.WORKSPACE = pwd()
         def projectName = new File(pomPath).parent
         baseDir = "${env.WORKSPACE}/$projectName"
