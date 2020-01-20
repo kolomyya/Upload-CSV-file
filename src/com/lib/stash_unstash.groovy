ccf12a08cc9251f516280f91b4d5474f25f6723c
kolomyya <44345671+kolomyya@users.noreply.github.com>
2020-01-19 22:07:05 -0600

    Update stash_unstash.groovy

--- src/com/lib/stash_unstash.groovy
+++ src/com/lib/stash_unstash.groovy
@@ -1,7 +1,7 @@
 node('slave') {
    node('master'){
     dir('path to file'){
-    stash name: 'my_stash' includes:'My.csv'
+       stash name: 'my_stash' includes{'My.csv'}
 }
 unstash name:'my_stash'
 sh 'ls'
