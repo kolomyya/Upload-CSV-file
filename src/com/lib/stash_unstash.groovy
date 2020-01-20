a5ec6f49f3d8ab58859987d5c3aad1b50a5828dc
kolomyya <44345671+kolomyya@users.noreply.github.com>
2020-01-19 22:35:21 -0600

    Update stash_unstash.groovy

--- src/com/lib/stash_unstash.groovy
+++ src/com/lib/stash_unstash.groovy
@@ -1,13 +1,26 @@
 node('slave') {
-   node('master'){
-      stage('Run') {
-    def stash_file(file_name, name){
-    this.steps.stash ('includes': file_name, 'name': name)
-}
+      stage('Stash') {
+        dir ('test') {
+            sh '''
+                cd file
+                touch My.csv
+                ls -al
+            '''
 
-def unstash_file(name){
-    this.steps.unstash name
-}
-   }
-}
-}
+            stash name: 'stash_test', includes: 'file/My.csv'
+
+        } // end dir
+    } // end stage 'Stash'
+
+    stage('Check unstash') {
+        dir('check') {
+            unstash 'stash_test'
+
+            sh 'tree -L 2'
+        }
+    }
+
+    stage('Clean up') {
+        sh 'tree -L 2'
+        deleteDir()
+    }
